363f12791e19089a55ebf48c4de1eb8cbadc9422
srcallah <44887032+srcallah@users.noreply.github.com>
2022-03-23 11:31:02 -0400

    updating index page

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
+
 var camera, scene, renderer, mesh;
 var mouseX = 0, mouseY = 0;
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
-var colors = ["#ffffff","#000000"];
+var colors = ["orchid", "#ffffff", "lime", "red", "gold", "mediumblue"];
 
 function init() {
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = -100;
-    camera.position.y = -100;
+    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = -90;
+    camera.position.y = -90;
     // 
-    var imgs = textToTexture("Mirror is a portal");
+    var imgs = textToTexture("Mirror as portal");
     var texture = new THREE.CubeTextureLoader().load(imgs);
     texture.mapping = THREE.CubeRefractionMapping;
 
@@ -21,10 +22,10 @@ function init() {
     var ambient = new THREE.AmbientLight(0xffffff);
     scene.add(ambient);
 
-    var geometry = new THREE.IcosahedronGeometry(60, 7);
+    var geometry = new THREE.IcosahedronGeometry(60, 4);
     var material = new THREE.MeshPhongMaterial({
         envMap: texture,
-        refractionRatio: 0.75
+        refractionRatio: 0.7
     });
 	
     mesh = new THREE.Mesh(geometry, material);
@@ -36,16 +37,16 @@ function init() {
     });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    
+    const sectionTag = document.querySelector("nav")
+    sectionTag.appendChild(renderer.domElement)
 
     document.addEventListener('mousemove', onDocumentMouseMove, false);
     document.addEventListener('touchmove', onDocumentMouseMove, false);
-    // window["text-input"].addEventListener('keyup', onTextChange, false);
     window.addEventListener('resize', onWindowResize, false);
 }
 
 function onTextChange() {
-    // var str = window["text-input"].value || "Hola Mundo";
     var imgs = textToTexture(str);
     var texture = new THREE.CubeTextureLoader().load(imgs);
     texture.mapping = THREE.CubeRefractionMapping;
@@ -62,7 +63,7 @@ function onWindowResize() {
 
 function onDocumentMouseMove(event) {
     if (event.touches) event = event.touches[0];
-    mouseX = (event.clientX - windowHalfX) / 4;
+    mouseX = (event.clientX - windowHalfX) / 6;
     mouseY = (event.clientY - windowHalfY) / 4;
 }
 
@@ -79,16 +80,15 @@ function render() {
 }
 
 function textToTexture(text) {
-    if (text === undefined) text = "Hello World";
     var canvas = document.createElement('canvas');
     var tile_size = 1024;
-    var fontSize = 50;
+    var fontSize = 60;
     // 
     canvas.width = tile_size * 4;
     canvas.height = tile_size;
     // 
     var ctx = canvas.getContext('2d');
-    ctx.font = fontSize + "px cursive, Sans-Serif";
+    ctx.font = fontSize + "px Old Standard TT";
     var fillColor = colors[Math.floor(Math.random()*colors.length)];
     document.body.style.backgroundColor = fillColor;
     ctx.fillStyle = fillColor;
@@ -126,4 +126,3 @@ function textToTexture(text) {
 
 init();
 animate();
-
--- js/plugins.js
+++ js/plugins.js
@@ -1,24 +1,70 @@
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-  var method;
-  var noop = function () {};
-  var methods = [
-    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-  ];
-  var length = methods.length;
-  var console = (window.console = window.console || {});
+document.addEventListener('DOMContentLoaded', function () {
+  const table = document.getElementById('sortTable');
+  const headers = table.querySelectorAll('th');
+  const tableBody = table.querySelector('tbody');
+  const rows = tableBody.querySelectorAll('tr');
 
-  while (length--) {
-    method = methods[length];
+  // track sorting
+  const directions = Array.from(headers).map(function (header) {
+      return '';
+  });
 
-    // Only stub undefined methods.
-    if (!console[method]) {
-      console[method] = noop;
-    }
-  }
-}());
+  // convert the content of given cell in given column
+  const transform = function (index, content) {
+      // Get the data type of column
+      const type = headers[index].getAttribute('data-type');
+      switch (type) {
+          case 'number':
+              return parseFloat(content);
+          case 'string':
+          default:
+              return content;
+      }
+  };
 
-// Place any jQuery/helper plugins in here.
+  const sortColumn = function (index) {
+      // Get the current direction
+      const direction = directions[index] || 'asc';
+
+      // A factor based on the direction
+      const multiplier = direction === 'asc' ? 1 : -1;
+
+      const newRows = Array.from(rows);
+
+      newRows.sort(function (rowA, rowB) {
+          const cellA = rowA.querySelectorAll('td')[index].innerHTML;
+          const cellB = rowB.querySelectorAll('td')[index].innerHTML;
+
+          const a = transform(index, cellA);
+          const b = transform(index, cellB);
+
+          switch (true) {
+              case a > b:
+                  return 1 * multiplier;
+              case a < b:
+                  return -1 * multiplier;
+              case a === b:
+                  return 0;
+          }
+      });
+
+      // Remove old rows
+      [].forEach.call(rows, function (row) {
+          tableBody.removeChild(row);
+      });
+
+      // Reverse the direction
+      directions[index] = direction === 'asc' ? 'desc' : 'asc';
+
+      // Append new row
+      newRows.forEach(function (newRow) {
+          tableBody.appendChild(newRow);
+      });
+  };
+
+  [].forEach.call(headers, function (header, index) {
+      header.addEventListener('click', function () {
+          sortColumn(index);
+      });
+  });
+});
